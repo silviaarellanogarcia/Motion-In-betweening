Corrected Q normalization andd write_bvh method

inbetweening/data_processing/utils.py
@@ -116,8 +116,8 @@ def compute_global_positions_in_a_sample(X, Q, parents):
     X_global = np.zeros((n_frames, n_joints, pos_dims))  # Initialize global positions
 
     if isinstance(X, torch.Tensor):
-        X = X.detach().numpy()
-        Q = Q.detach().numpy()
+        X = X.detach().cpu().numpy()
+        Q = Q.detach().cpu().numpy()
 
     for frame in range(n_frames):
         for joint in range(n_joints):

inbetweening/model/diffusion.py
@@ -106,9 +106,6 @@ class DiffusionModel(pl.LightningModule):
 
         noisy_Q_0 = Q_0.clone()
         noisy_Q_0[:, masked_frames, :, :] = (sqrt_alphas_cumprod_t_Q.to(self.device) * Q_0.to(self.device) + sqrt_one_minus_alphas_cumprod_t_Q.to(self.device) * noise_Q.to(self.device))[:, masked_frames, :, :].float()
-
-        # Normalize the quaternions to ensure they are valid unit quaternions
-        noisy_Q_0 = F.normalize(noisy_Q_0, dim=-1)
         
         return noisy_X_0, noisy_Q_0, noise_X, noise_Q
     
@@ -257,6 +254,9 @@ class DiffusionModel(pl.LightningModule):
         print("Denoised sequences X:", noisy_X_0[:, :, 0, :])
         print("Denoised sequences Q:", noisy_Q_0.shape)
 
+        # Normalize the quaternions to ensure they are valid unit quaternions
+        noisy_Q_0 = F.normalize(noisy_Q_0, dim=-1) ## TODO: Check that the samples that weren't modified remain the same
+
         return noisy_X_0[0], noisy_Q_0[0]
 
 
@@ -289,7 +289,8 @@ if __name__ == "__main__":
                  trainer_defaults={
                      'logger': logger_config,
                      'callbacks': [checkpoint_callback],
+                    #  'overfit_batches': 1 ## TODO: AT SOME POINT REMOVE THE OVERFITTING
     })
 
-    ## COMMAND: python diffusion.py fit --config ./default_config.yaml
+    ## COMMAND: python diffusion.py fit --config ./config.yaml
     ## For continue training from a checkpoint: python diffusion.py fit --config ./default_config.yaml --ckpt_path PATH

inbetweening/model/generate_samples.py
@@ -26,7 +26,7 @@ n_joints = config['model']['n_joints']
 down_channels = config['model']['down_channels']
 
 model = DiffusionModel.load_from_checkpoint(
-    '/proj/diffusion-inbetweening/inbetweening/model/lightning_logs/my_model_init/version_24/checkpoints/epoch=617-step=148938.ckpt',
+    '/proj/diffusion-inbetweening/inbetweening/model/lightning_logs/my_model_init/version_25/checkpoints/epoch=480-step=115921.ckpt',
     beta_start=beta_start,
     beta_end=beta_end,
     n_diffusion_timesteps=n_diffusion_timesteps,
@@ -70,8 +70,8 @@ X_denoised_global = compute_global_positions_in_a_sample(denoised_X, denoised_Q,
 # plot_3d_skeleton_with_lines(torch.tensor(X_denoised_global).unsqueeze(0), sample['parents'], sequence_index=0, frames_range=(24, 25))
 
 # Plot the root's path
-plot_root(sample['X'].unsqueeze(0)[:, :, 0, :].detach().numpy(), start_frame=0, end_frame=49, sequence_index=0)
-plot_root(denoised_X.unsqueeze(0)[:, :, 0, :].detach().numpy(), start_frame=0, end_frame=49, sequence_index=0)
+# plot_root(sample['X'].unsqueeze(0)[:, :, 0, :].detach().numpy(), start_frame=0, end_frame=49, sequence_index=0)
+# plot_root(denoised_X.unsqueeze(0)[:, :, 0, :].detach().numpy(), start_frame=0, end_frame=49, sequence_index=0)
 
 # Generate BVH files
 write_bvh('output_original.bvh', X=sample['X'], Q_global=sample['Q'], parents=sample['parents'])