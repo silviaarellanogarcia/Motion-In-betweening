b556b0cf67fd2e2953c7adf1a89cdfcbd3afa698
Silvia Arellano García <silviaarellanogarcia@gmail.com>
2024-11-11 08:12:11 +0100

    Correction of the interpolation method to fit the data

--- inbetweening/evaluation/baseline.py
+++ inbetweening/evaluation/baseline.py
@@ -3,40 +3,42 @@ import torch
 from inbetweening.data_processing.process_data import Lafan1Dataset
 from scipy.spatial.transform import Rotation as R
 from scipy.spatial.transform import Slerp
+import pymotion.rotations.ortho6d as sixd
 
 from inbetweening.utils.convert_to_bvh import write_bvh
 
-
-def interpolate_position(past_context_seq, target_sample, num_gap_frames):
-    last_position = past_context_seq[-1]
+def interpolate_position(X, masked_frames):
+    num_gap_frames = len(masked_frames)
 
     # Initialize an array to hold the new interpolated positions
-    new_seq = np.zeros((num_gap_frames, last_position.shape[0], last_position.shape[1]))
-    new_seq[:, 1:, :] = last_position[1:, :] ## Set all the offsets. Now, only the hip movements are missing
+    new_seq_X = X.clone()
+    new_seq_X[masked_frames, 0, :] = 0 ## Set all the offsets. Now, only the hip movements are missing
+
+    last_positions = X[masked_frames[0] - 1, 0, :]
+    target_positions = X[masked_frames[-1] + 1, 0, :]
 
     for frame in range(num_gap_frames):
         # Calculate the interpolation factor
         alpha = (frame + 1) / (num_gap_frames + 1)  # Normalized factor between 0 and 1
         
         # Linearly interpolate between the last position and the target sample
-        new_pos = (1 - alpha) * last_position[0, :] + alpha * target_sample[0, :]
+        new_pos = (1 - alpha) * last_positions + alpha * target_positions
         
         # Store the new position in the new sequence
-        new_seq[frame, 0] = new_pos
+        new_seq_X[masked_frames[0] + frame, 0, :] = new_pos
 
-    # Concatenate past_context_seq, new_seq, and target_sample
-    final_seq = np.concatenate((past_context_seq, new_seq, target_sample[np.newaxis, :]), axis=0)
+    return new_seq_X
 
-    return final_seq
-
-
-def interpolate_quaternions(Q, start_frame, target_frame, num_gap_frames):
+def interpolate_quaternions(Q, masked_frames):
     num_joints = Q.shape[1]
+    start_frame = masked_frames[0] - 1
+    target_frame = masked_frames[-1] + 1
+    num_gap_frames = len(masked_frames)
     
     for joint in range(num_joints):
         # Get quaternions for the start and end frames for the current joint
-        quat_start = Q[start_frame, joint]
-        quat_end = Q[target_frame, joint]
+        quat_start = Q[start_frame, joint, :]
+        quat_end = Q[target_frame, joint, :]
         
         # The SLERP interpolator works with the Rotation class!
         # Create the rotations and the SLERP interpolator
@@ -51,26 +53,35 @@ def interpolate_quaternions(Q, start_frame, target_frame, num_gap_frames):
         gap_frames = range(start_frame + 1, target_frame)
         # Replace the missing frames in Q with the interpolated quaternions
         for idx, frame in enumerate(gap_frames):
-            Q[frame, joint] = torch.from_numpy(interpolated_rots[idx].as_quat())
+            Q[frame, joint, :] = torch.from_numpy(interpolated_rots[idx].as_quat())
 
     return Q
 
 
 if __name__ == "__main__":
-    bvh_path = "/Users/silviaarellanogarcia/Documents/MSc MACHINE LEARNING/Advanced Project/proyecto/data_walking"  # Update this with the actual path
-    dataset = Lafan1Dataset(bvh_path, window=50, offset=20, train=True)
+    bvh_path = "../../data"  # Update this with the actual path
+    dataset = Lafan1Dataset(bvh_path, window=50, offset=20, train=True, scaling=1)
 
     # Test by retrieving a sample from the dataset
-    sample_idx = 0  # Test with the first sample
+    sample_idx = 25  # Test with the first sample
     sample = dataset[sample_idx]
+
+    mean_X = torch.tensor(dataset.training_mean_X, dtype=torch.float32).cpu()
+    std_X = torch.tensor(dataset.training_std_X, dtype=torch.float32).cpu()
+    sample_X_original = sample['X']/1 * std_X + mean_X
+
+    Q = sample['Q'].reshape(sample['Q'].shape[0], sample['Q'].shape[1], 3, 2) # Shape (frames, joints, 6)
+    Q = Q.numpy()
+    Q = sixd.to_quat(Q) # Shape (frames, joints, 3, 2)
+    Q = torch.from_numpy(Q).float()
     
-    write_bvh('output_walk_gt2.bvh', sample['X'], sample['parents'], Q_global=sample['Q'])
+    write_bvh('output_walk_gt2.bvh', sample_X_original, sample['parents'], Q_global=Q)
 
-    past_context_seq = sample['X'][:10, :, :]
-    target_sample = sample['X'][15, :, :]
-    num_gap_frames = 5
+    masked_frames = [20, 21, 22, 23, 24, 25]
 
-    interpolated_X = interpolate_position(past_context_seq, target_sample, num_gap_frames)
-    interpolated_Q = interpolate_quaternions(sample['Q'], 9, 15, 5)
+    sample_X_original[masked_frames, 0, :] = 0
+    Q[masked_frames, :, :] = 0
+    interpolated_X = interpolate_position(sample_X_original, masked_frames)
+    interpolated_Q = interpolate_quaternions(Q, masked_frames)
 
-    write_bvh('output_walk_int2.bvh', interpolated_X, sample['parents'], Q_global=interpolated_Q[:16, :, :])
+    write_bvh('output_walk_int2.bvh', interpolated_X, sample['parents'], Q_global=interpolated_Q)
