f3f1fbd86fa4c97180ed93534083870a5f2f9ca0
Silvia Arellano García <silviaarellanogarcia@gmail.com>
2024-11-19 19:00:40 +0100

    LSTM model for motion inbetweening (baseline)

---
+++ inbetweening/regression_model/LSTM_generate_samples.py
@@ -0,0 +1,82 @@
+import torch
+from pytorch_lightning import Trainer
+from pytorch_lightning.utilities.model_summary import summarize
+import yaml
+
+from inbetweening.data_processing.process_data import Lafan1DataModule
+from inbetweening.evaluation.baseline import full_interpolation
+from inbetweening.model.diffusion import DiffusionModel
+from inbetweening.regression_model.LSTM_model import MotionLSTM
+from inbetweening.utils.aux_functions import plot_3d_skeleton_with_lines, plot_root
+from inbetweening.data_processing.utils import compute_global_positions_in_a_sample
+from inbetweening.utils.convert_to_bvh import write_bvh
+import pymotion.rotations.ortho6d as sixd
+
+path_to_checkpoint = '/proj/diffusion-inbetweening/inbetweening/regression_model/lightning_logs/motionLSTM/version_0/checkpoints/epoch=979-step=59780.ckpt'
+config_file_corresponding_to_ckpt = '/proj/diffusion-inbetweening/inbetweening/regression_model/lightning_logs/motionLSTM/version_0/config.yaml'
+
+# Load the config file
+with open(config_file_corresponding_to_ckpt, 'r') as f:
+    config = yaml.safe_load(f)
+
+# Prepare all the hyperparameters that are necessary for the model
+lr = config['model']['lr']
+gap_size = 5
+type_masking = config['model']['type_masking']
+window = config['data']['window']  # This corresponds to the 'window' value in the config
+offset = config['data']['offset']
+step_threshold = config['model']['step_threshold']
+max_gap_size = config['model']['max_gap_size']
+hidden_size = config['model']['hidden_size']
+
+model = MotionLSTM.load_from_checkpoint(
+    path_to_checkpoint,
+    hidden_size=hidden_size,
+    lr=lr,
+    gap_size=gap_size,
+    type_masking=type_masking,
+    n_frames=window,
+    step_threshold=step_threshold, 
+    max_gap_size=max_gap_size
+)
+
+data_module = Lafan1DataModule(
+    data_dir='/proj/diffusion-inbetweening/data',
+    batch_size=1,
+    window=window,
+    offset=offset,
+)
+
+data_module.setup(stage='test')
+
+# Get a single sample from the test dataset
+sample_index = 2500  # Adjust this index as needed
+test_dataset = data_module.test_dataset
+sample = test_dataset[sample_index]
+sample = {key: value.to(model.device) for key, value in sample.items()}
+
+# ATTENTION! The Q is global.
+predicted_Q, masked_frames = model.generate_samples(sample['X'], sample['Q'], model)
+
+print(masked_frames)
+
+print('Inbetweening finished!')
+
+print("ORIGINAL SAMPLE Q: ", sample['Q'][masked_frames,0,:])
+print("DENOISED SAMPLE Q: ", predicted_Q[masked_frames,0,:])
+
+# Pass from Ortho6D to quaternions
+original_Q_quat = sample['Q'].reshape(sample['Q'].shape[0], sample['Q'].shape[1], 3, 2) # Shape (frames, joints, 6) --> Shape (frames, joints, 3, 2)
+original_Q_quat = sixd.to_quat(original_Q_quat.cpu().numpy())  # Convert to NumPy for sixd
+
+predicted_Q_quat = predicted_Q.reshape(predicted_Q.shape[0], predicted_Q.shape[1], 3, 2)
+predicted_Q_quat = sixd.to_quat(predicted_Q_quat.cpu().numpy())  # Convert to NumPy for sixd
+
+# Convert back to PyTorch tensors
+original_Q_quat = torch.tensor(original_Q_quat, device=sample['X'].device)
+predicted_Q_quat = torch.tensor(predicted_Q_quat, device=sample['X'].device)
+
+
+# Generate BVH files
+write_bvh('LSTM_1_original.bvh', X=sample['X'], Q_global=original_Q_quat, parents=sample['parents'])
+write_bvh('LSTM_1_denoised_15_fr.bvh', X=sample['X'], Q_global=predicted_Q_quat, parents=sample['parents'])
---
+++ inbetweening/regression_model/LSTM_model.py
@@ -0,0 +1,192 @@
+import torch
+import torch.nn as nn
+import pytorch_lightning as pl
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.cli import LightningCLI
+
+from inbetweening.data_processing.process_data import Lafan1DataModule
+
+class StopTrainingOnGapSize(pl.Callback):
+    def __init__(self, gap_size_threshold=30):
+        self.gap_size_threshold = gap_size_threshold
+
+    def on_epoch_start(self, trainer, pl_module):
+        """Stop the training if gap_size is equal to or greater than the threshold."""
+        if pl_module.gap_size == self.gap_size_threshold:
+            print(f"Stopping training because gap_size is {self.gap_size_threshold}.")
+            trainer.should_stop = True  # This stops the training process
+
+class MotionLSTM(pl.LightningModule):
+    def __init__(self, hidden_size: int = 128, lr: float = 0.001, gap_size: int = 1, type_masking: str = 'continued', n_frames: int = 50, step_threshold: int = 8000, max_gap_size: int = 15):
+        super(MotionLSTM, self).__init__()
+        self.learning_rate = lr
+        self.gap_size = gap_size
+        self.max_gap_size = max_gap_size
+        self.type_masking = type_masking
+        self.n_frames = n_frames
+        self.steps_since_last_gap_increase = 0
+        self.step_threshold = step_threshold
+
+        # Temporal processing (Shared across root positions and joint rotations)
+        self.temporal_processing = nn.LSTM(input_size=n_frames, hidden_size=hidden_size, num_layers=2, batch_first=True)
+
+        # Final prediction layer (for each joint's 6D rotation in the masked frames)
+        self.output_layer = nn.Linear(hidden_size, self.n_frames)
+
+
+    def forward(self, X, Q):
+        """Forward pass through the network."""
+
+        batch_size, frames, joints, pos_dims = X.shape
+        X = X.view(batch_size, frames, joints * pos_dims)  # Flatten positions (X)
+
+        batch_size, frames, joints, angle_dims = Q.shape
+        Q = Q.view(batch_size, frames, joints * angle_dims)  # Flatten Ortho6D angles (Q)
+
+        # Concatenate positions (X) and Ortho6D angles (Q) along the joint dimension
+        X_and_Q = torch.cat((X, Q), dim=2)  # X and Q are concatenated along the channels axis
+        X_and_Q = torch.permute(X_and_Q, (0, 2, 1))  # Change to (batch_size, channels, frames)
+
+        # Feed combined features into LSTM for temporal processing
+        lstm_out, _ = self.temporal_processing(X_and_Q)
+
+        # Output layer for predicting quaternions for each joint
+        output = self.output_layer(lstm_out)  # (batch_size, num_joints*9, frames)
+        output = output.permute(0, 2, 1) # (batch_size, frames, num_joints*9)
+        output = output.view(batch_size, frames, joints, 9)
+
+        return output[:, :, :, 3:]
+
+    
+    def masking(self, n_frames, gap_size, type='continued'):
+        """
+        Masks the information of some frames in a motion sequence.
+        Type can be 'continued' (the masked frames are one after the other) or 'spread' (the masked frames are placed randdomly on the sequence, except first and last position).
+        """
+        ## Shape of X: batch_size, frames, joints, position_dims
+        ## Shape of Q: batch_size, frames, joints, quaternnion_dims
+
+        masked_frames = []
+
+        if gap_size > n_frames - 2:
+            raise AssertionError('Your gap size is bigger or equal than the number of frames in your sequence minus 2.')
+        if gap_size/n_frames >= 0.7:
+            print('Attention!! You are masking more than the 70% of frames!')
+
+        if type == 'continued':
+            start_frame = int((n_frames - gap_size) / 2)
+            masked_frames = list(range(start_frame, start_frame + gap_size))
+        
+        elif type == 'spread':
+            # Selection of 'gap_size' frames to mask, excluding the first and last frames
+            masked_frames = torch.randperm(n_frames - 2)[:gap_size] + 1  # +1 to exclude frame 0
+            masked_frames = masked_frames.tolist()  # Convert to list
+            masked_frames = sorted(set(masked_frames))
+
+        return masked_frames
+
+    def training_step(self, batch, batch_idx):
+        """Perform a training step."""
+        if self.steps_since_last_gap_increase >= self.step_threshold:  # TODO: Change 5 with a parameter indicating the maximum gap
+            self.gap_size = min(self.gap_size + 1, self.max_gap_size)  # TODO: Adjust maximum gap parameter
+            self.step_threshold += 3000
+            self.steps_since_last_gap_increase = 0
+
+        X = batch['X']
+        Q = batch['Q']
+
+        # Masking
+        masked_frames = self.masking(n_frames=Q.shape[1], gap_size=self.gap_size, type=self.type_masking)
+
+        masked_Q = Q.clone()
+        masked_Q[:, masked_frames, :, :] = 0.0
+
+        # Forward pass
+        predicted_rotation = self(X, masked_Q)
+
+        # Compute loss
+        loss_rotation = nn.MSELoss()(predicted_rotation[:, masked_frames, :, :], Q[:, masked_frames, :, :])
+
+        # Log losses
+        self.log('train_loss', loss_rotation, prog_bar=True)
+        self.log('gap_size', self.gap_size, prog_bar=False)
+
+        self.steps_since_last_gap_increase += 1
+
+        return loss_rotation
+    
+    def validation_step(self, batch, batch_idx):
+        """Perform a training step."""
+        X = batch['X']
+        Q = batch['Q']
+
+        # Masking
+        masked_frames = self.masking(n_frames=Q.shape[1], gap_size=self.gap_size, type=self.type_masking)
+
+        masked_Q = Q.clone()
+        masked_Q[:, masked_frames, :, :] = 0.0
+
+        # Forward pass
+        predicted_rotation = self(X, masked_Q)
+
+        # Compute loss
+        loss_rotation = nn.MSELoss()(predicted_rotation[:, masked_frames, :, :], Q[:, masked_frames, :, :])
+
+        # Log losses
+        self.log('validation_loss', loss_rotation, prog_bar=True)
+
+        return loss_rotation
+
+    def configure_optimizers(self):
+        """Define the optimizer and learning rate scheduler."""
+        optimizer = torch.optim.Adam(self.parameters(), lr=self.learning_rate)
+        return optimizer
+    
+    def generate_samples(self, X_0, Q_0, model):
+        self.eval()
+        with torch.no_grad():
+            X_0 = X_0.unsqueeze(0)
+            Q_0 = Q_0.unsqueeze(0) ## This adds the batch dimension
+
+            masked_frames = self.masking(n_frames=Q_0.shape[1], gap_size=self.gap_size, type=self.type_masking)
+
+            masked_Q = Q_0.clone()
+            masked_Q[:, masked_frames, :, :] = 0.0
+
+            # Calculate loss
+            Q_pred = model(X_0, masked_Q) ### I will have X and Q together and I have to separate them.
+            masked_Q[:, masked_frames, :, :] = Q_pred[:, masked_frames, :, :].float()
+
+        return masked_Q[0], masked_frames
+
+
+if __name__ == '__main__':
+    print("AM I USING GPU? ", torch.cuda.is_available())
+    logger_config = {
+        'class_path': 'pytorch_lightning.loggers.TensorBoardLogger',
+        'init_args': {                      # Use init_args instead of params
+            'save_dir': 'lightning_logs',
+            'name': 'motionLSTM',
+            'version': None
+        }
+    }
+
+    checkpoint_callback = ModelCheckpoint(
+        save_top_k=10,  # Keep 10 checkpoints
+        monitor='train_loss',
+        mode="min"
+    )
+
+    stop_training_callback = StopTrainingOnGapSize(gap_size_threshold=30)
+
+    # Use LightningCLI with the updated logger configuration
+    LightningCLI(MotionLSTM, 
+                 Lafan1DataModule, 
+                 trainer_defaults={
+                     'logger': logger_config,
+                     'callbacks': [checkpoint_callback, stop_training_callback],
+                    #  'overfit_batches': 1 ## TODO: AT SOME POINT REMOVE THE OVERFITTING
+    })
+
+    ## COMMAND: python LSTM_model.py fit --config ./LSTM_config.yaml
+    ## For continue training from a checkpoint: python motion_regressor.py fit --config ./config_regressor.yaml --ckpt_path PATH
